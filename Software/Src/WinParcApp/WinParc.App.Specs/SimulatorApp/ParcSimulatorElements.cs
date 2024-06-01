a5c3b64fc3afe79e1150781f72f698578535d8be
mrstefangrimm <motionsystems@grimm.com>
2024-06-01 12:06:57 +0200

    Fix manual PUTTY input. (#11)
    * Adds missing return statement and re-enables optimizer.
    
    * Replace WinAppDriver with FlaUI
    
    ---------
    
    Co-authored-by: mrstefangrimm <motionssystems@grimm.com>

--- Software/Src/WinParcApp/WinParc.App.Specs/SimulatorApp/ParcSimulatorElements.cs
+++ Software/Src/WinParcApp/WinParc.App.Specs/SimulatorApp/ParcSimulatorElements.cs
@@ -1,40 +1,40 @@
-﻿// Copyright (c) 2023 Stefan Grimm. All rights reserved.
+﻿// Copyright (c) 2023-2024 Stefan Grimm. All rights reserved.
 // Licensed under the LGPL. See LICENSE file in the project root for full license information.
 //
-using OpenQA.Selenium.Appium.Windows;
-using SpecFlow.Actions.WindowsAppDriver;
+using FlaUI.Core.AutomationElements;
+using Futile.Specflow.Actions.FlaUI;
 
 namespace WinParc.App.Specs.SimulatorApp {
 
   internal class ParcSimulatorElements {
 
-    protected readonly AppDriver _appDriver;
+    protected readonly FlaUIDriver _driver;
 
-    public ParcSimulatorElements(AppDriver appDriver) {
-      _appDriver = appDriver;
+    public ParcSimulatorElements(FlaUIDriver driver) {
+      _driver = driver;
     }
 
-    public WindowsElement SendText => _appDriver.Current.FindElementByAccessibilityId("textbox_SendText");
+    public TextBox SendText => _driver.Current.FindFirstDescendant("textbox_SendText").AsTextBox();
 
-    public WindowsElement SendPressCmd => _appDriver.Current.FindElementByAccessibilityId("button_SendPressCmd");
+    public Button SendPressCmd => _driver.Current.FindFirstDescendant("button_SendPressCmd").AsButton();
 
-    public WindowsElement TerminalOutput => _appDriver.Current.FindElementByAccessibilityId("textbox_TerminalOutput");
+    public TextBox TerminalOutput => _driver.Current.FindFirstDescendant("textbox_TerminalOutput").AsTextBox();
 
-    public WindowsElement DebugOutput => _appDriver.Current.FindElementByAccessibilityId("textbox_DebugOutput");
+    public TextBox DebugOutput => _driver.Current.FindFirstDescendant("textbox_DebugOutput").AsTextBox();
 
-    public WindowsElement ButtonA => _appDriver.Current.FindElementByAccessibilityId("button_A");
-    public WindowsElement ButtonB => _appDriver.Current.FindElementByAccessibilityId("button_B");
-    public WindowsElement ButtonC => _appDriver.Current.FindElementByAccessibilityId("button_C");
-    public WindowsElement ButtonD => _appDriver.Current.FindElementByAccessibilityId("button_D");
-    public WindowsElement ButtonE => _appDriver.Current.FindElementByAccessibilityId("button_E");
+    public Button ButtonA => _driver.Current.FindFirstDescendant("button_A").AsButton();
+    public Button ButtonB => _driver.Current.FindFirstDescendant("button_B").AsButton();
+    public Button ButtonC => _driver.Current.FindFirstDescendant("button_C").AsButton();
+    public Button ButtonD => _driver.Current.FindFirstDescendant("button_D").AsButton();
+    public Button ButtonE => _driver.Current.FindFirstDescendant("button_E").AsButton();
 
-    public WindowsElement CheckBoxM0 => _appDriver.Current.FindElementByAccessibilityId("checkbox_M0");
-    public WindowsElement CheckBoxM1 => _appDriver.Current.FindElementByAccessibilityId("checkbox_M1");
+    public CheckBox CheckBoxM0 => _driver.Current.FindFirstDescendant("checkbox_M0").AsCheckBox();
+    public CheckBox CheckBoxM1 => _driver.Current.FindFirstDescendant("checkbox_M1").AsCheckBox();
 
-    public WindowsElement CheckBoxC4 => _appDriver.Current.FindElementByAccessibilityId("checkbox_C4");
-    public WindowsElement CheckBoxC3 => _appDriver.Current.FindElementByAccessibilityId("checkbox_C3");
-    public WindowsElement CheckBoxC2 => _appDriver.Current.FindElementByAccessibilityId("checkbox_C2");
-    public WindowsElement CheckBoxC1 => _appDriver.Current.FindElementByAccessibilityId("checkbox_C1");
+    public CheckBox CheckBoxC4 => _driver.Current.FindFirstDescendant("checkbox_C4").AsCheckBox();
+    public CheckBox CheckBoxC3 => _driver.Current.FindFirstDescendant("checkbox_C3").AsCheckBox();
+    public CheckBox CheckBoxC2 => _driver.Current.FindFirstDescendant("checkbox_C2").AsCheckBox();
+    public CheckBox CheckBoxC1 => _driver.Current.FindFirstDescendant("checkbox_C1").AsCheckBox();
 
   }
 }
